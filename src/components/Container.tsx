f743288a47a463c25daeccfc180c310e86cc2be3
Eche00 <echeeze956@gmail.com>
2025-08-10 19:14:43 +0100

    Container to place things

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,16 @@
 import Header from "./Header"
 import { Outlet } from "react-router";
 import  Footer from "./Footer"
+// import { Svgs } from "../lib/svg/Svg";
 
 function Container() {
   return (
-    <div>
-      <Header/>
+    <div className="relative">
+    <Header/>
+    <div className="pt-[90px] ">
       <Outlet/>
+
+    </div>
       <Footer/>
     </div>
   )
