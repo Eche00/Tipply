fb4b287f71a5c854348fcbeec9a6c57059304420
Eche00 <echeeze956@gmail.com>
2025-08-11 20:53:35 +0100

    added container segmants

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,14 @@
 import Header from "./Header"
 import { Outlet } from "react-router";
-import  Footer from "./Footer"
-// import { Svgs } from "../lib/svg/Svg";
 
 function Container() {
   return (
     <div className="relative">
     <Header/>
+    
     <div className="pt-[90px] ">
       <Outlet/>
-
     </div>
-      <Footer/>
     </div>
   )
 }
