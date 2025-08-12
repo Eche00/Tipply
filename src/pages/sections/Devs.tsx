26918cd7996dcd8c21ea458795d319d54dad06ec
Eche00 <echeeze956@gmail.com>
2025-08-12 20:19:55 +0100

    Devs ui display page for export

---
+++ src/pages/sections/Devs.tsx
@@ -0,0 +1,219 @@
+import { Link } from "react-router";
+import { Icons } from "../../lib/icons/Icons";
+
+function Devs() {
+  return (
+    <div className="    bg-[linear-gradient(to_right,#4f4f4f0e_0.8px,transparent_0.1px),linear-gradient(to_bottom,#4f4f4f0e_0.8px,transparent_0.1px)] md:bg-[size:104px_104px] bg-[size:50px_50px]  [mask-image:radial-gradient(ellipse_100%_70%_at_50%_100%,#000_70%,transparent_[200%])]   h-fit  flex  items-center justify-center  w-full ">
+      <section className="flex flex-col w-[90%] mx-auto py-[50px] relative z-30">
+        <div className="flex sm:flex-row flex-col sm:items-start items-center sm:justify-between justify-center">
+          <h2 className="  text-[40px]  font-[400] leading-[55px] capitalize bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF69] bg-clip-text text-transparent text-center">
+            Core Values
+          </h2>
+
+          <p className=" text-[#AFB3CA] text-[16px] font-[400] leading-[24px] sm:text-start text-center max-w-[400px]">
+            We believe in transparency, innovation, and community-driven growth.
+            Our commitment is to empower developers and supporters alike with
+            privacy, security, and seamless interactions across the Web3
+            ecosystem.
+          </p>
+        </div>
+
+        <section className="flex  flex-wrap gap-[48px] sm:justify-start justify-center py-[48px] ">
+          {/* mission  */}
+          <section className=" bg-[#141414] rounded-[24px] flex flex-col gap-[5px] overflow-hidden w-[320px] group hover:scale-[102%] transition-all duration-300">
+            <div className=" w-full h-fit ">
+              <img
+                src="/logo.png"
+                alt=""
+                className=" w-full h-[150px] hover:scale-[105%]  object-cover duration-300"
+              />
+            </div>
+
+            <div className="w-[90%] mx-auto pb-[10px]">
+              <h3 className="text-[20px] text-[#FFFFFF] font-[400] italic">
+                @_Mission
+              </h3>
+              <p className="text-[14px] text-[#AFB3CA] font-[400] ">
+                To connect tippers and developers worldwide community growth.
+              </p>
+            </div>
+            <div className="w-[90%] mx-auto flex gap-[15px] items-center pb-[20px]">
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300 font-extrabold">
+                View
+              </button>
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b border-[#7346F1] text-[#7346F1] font-extrabold cursor-pointer hover:scale-[102%] transition-all duration-300">
+                𝓣ip
+              </button>
+            </div>
+          </section>
+          {/* mission  */}
+          <section className=" bg-[#141414] rounded-[24px] flex flex-col gap-[5px] overflow-hidden w-[320px] group hover:scale-[102%] transition-all duration-300">
+            <div className=" w-full h-fit ">
+              <img
+                src="/logo.png"
+                alt=""
+                className=" w-full h-[150px] hover:scale-[105%]  object-cover duration-300"
+              />
+            </div>
+
+            <div className="w-[90%] mx-auto pb-[10px]">
+              <h3 className="text-[20px] text-[#FFFFFF] font-[400] italic">
+                @_Mission
+              </h3>
+              <p className="text-[14px] text-[#AFB3CA] font-[400] ">
+                To connect tippers and developers worldwide community growth.
+              </p>
+            </div>
+            <div className="w-[90%] mx-auto flex gap-[15px] items-center pb-[20px]">
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                View
+              </button>
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                𝓣ip
+              </button>
+            </div>
+          </section>
+          {/* mission  */}
+          <section className=" bg-[#141414] rounded-[24px] flex flex-col gap-[5px] overflow-hidden w-[320px] group hover:scale-[102%] transition-all duration-300">
+            <div className=" w-full h-fit ">
+              <img
+                src="/logo.png"
+                alt=""
+                className=" w-full h-[150px] hover:scale-[105%]  object-cover duration-300"
+              />
+            </div>
+
+            <div className="w-[90%] mx-auto pb-[10px]">
+              <h3 className="text-[20px] text-[#FFFFFF] font-[400] italic">
+                @_Mission
+              </h3>
+              <p className="text-[14px] text-[#AFB3CA] font-[400] ">
+                To connect tippers and developers worldwide community growth.
+              </p>
+            </div>
+            <div className="w-[90%] mx-auto flex gap-[15px] items-center pb-[20px]">
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                View
+              </button>
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                𝓣ip
+              </button>
+            </div>
+          </section>
+          {/* mission  */}
+          <section className=" bg-[#141414] rounded-[24px] flex flex-col gap-[5px] overflow-hidden w-[320px] group hover:scale-[102%] transition-all duration-300">
+            <div className=" w-full h-fit ">
+              <img
+                src="/logo.png"
+                alt=""
+                className=" w-full h-[150px] hover:scale-[105%]  object-cover duration-300"
+              />
+            </div>
+
+            <div className="w-[90%] mx-auto pb-[10px]">
+              <h3 className="text-[20px] text-[#FFFFFF] font-[400] italic">
+                @_Mission
+              </h3>
+              <p className="text-[14px] text-[#AFB3CA] font-[400] ">
+                To connect tippers and developers worldwide community growth.
+              </p>
+            </div>
+            <div className="w-[90%] mx-auto flex gap-[15px] items-center pb-[20px]">
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                View
+              </button>
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                𝓣ip
+              </button>
+            </div>
+          </section>
+          {/* mission  */}
+          <section className=" bg-[#141414] rounded-[24px] flex flex-col gap-[5px] overflow-hidden w-[320px] group hover:scale-[102%] transition-all duration-300">
+            <div className=" w-full h-fit ">
+              <img
+                src="/logo.png"
+                alt=""
+                className=" w-full h-[150px] hover:scale-[105%]  object-cover duration-300"
+              />
+            </div>
+
+            <div className="w-[90%] mx-auto pb-[10px]">
+              <h3 className="text-[20px] text-[#FFFFFF] font-[400] italic">
+                @_Mission
+              </h3>
+              <p className="text-[14px] text-[#AFB3CA] font-[400] ">
+                To connect tippers and developers worldwide community growth.
+              </p>
+            </div>
+            <div className="w-[90%] mx-auto flex gap-[15px] items-center pb-[20px]">
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+               View
+              </button>
+              <button className="flex-1 py-[8px] bg-[#56565653] rounded-[13px] flex items-center justify-center border-b cursor-pointer hover:scale-[102%] transition-all duration-300">
+                𝓣ip
+              </button>
+            </div>
+          </section>
+
+          {/* support  */}
+          {/* <div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]">
+                    <span>{Icons.people}</span>
+                    <h3 className="text-[24px] text-[#FFFFFF] font-[400]">Connect. Support. Grow.</h3>
+                    <p className="max-w-[325px] text-[16px] text-[#AFB3CA] font-[400]">At Tipply, we believe the power of Web3 comes from people coming together. Connect with talented developers, support innovative projects through seamless tipping, and grow a vibrant community that drives the future of decentralized collaboration. Together, we create more than just technology, we build lasting impact.</p>
+                  </div> */}
+        </section>
+
+        {/* controller  */}
+        <section className="flex flex-col gap-[5px] py-[50px]">
+          <div className=" w-full flex items-center justify-center ">
+           <div className="w-fit flex items-center gap-[10px] bg-[#56565633] rounded-[8px]">
+             <p className=" px-[15px] py-[2px] rounded-[8px] bg-[#7346F1] cursor-pointer">1</p>
+            <p className=" px-[15px] py-[2px] rounded-[8px] cursor-pointer">2</p>
+            <p className=" px-[15px] py-[2px] rounded-[8px] cursor-pointer">3</p>
+           </div>
+          </div>
+          <div className="flex items-center justify-between">
+              <button className="w-[120px] sm:w-[300px] py-[16px] bg-[#56565653] rounded-[13px] flex items-center justify-center gap-[10px] border-b cursor-pointer hover:scale-[102%] transition-all duration-300 border-[#7346F1]">
+               {Icons.arrowleft}  Prev
+              </button>
+              <button className="w-[120px] sm:w-[300px] py-[16px] bg-[#56565653] rounded-[13px] flex items-center justify-center gap-[10px] border-b cursor-pointer hover:scale-[102%] transition-all duration-300 border-[#7346F1] f">
+                Next {Icons.arrowright} 
+              </button>
+              </div>
+        </section>
+        {/* Discover & Tip */}
+        <div className="flex sm:flex-row flex-col gap-[20px] bg-[#141414] rounded-[24px] sm:h-full sm:max-h-[377px] h-[752px] overflow-hidden ">
+          <div className="flex flex-col gap-[16px] sm:py-[61px] py-[30px] sm:pl-[70px] sm:pr-0 pr-[30px] pl-[30px] sm:flex-1 w-full">
+            <p className="bg-[#FFFFFF0A] px-[10px] py-[8px] border-[1px] shadow-sm  border-[#FFFFFF14] text-[#FFFFFF] text-[12px] font-extrabold leading-[16px] flex items-center justify-center gap-[10px] rounded-full uppercase w-[117px]">
+              DevSpot
+            </p>
+            <h3 className=" text-[#CAD1E9] text-[32px] font-[400]">
+              Discover & Tip
+            </h3>
+            <p className=" text-[#AFB3CA] text-[16px] font-[400] max-w-[750px]">
+              Discover talented developers shaping the Web3 space. Explore their
+              projects, tip your favorites, and connect directly to support
+              innovation.
+            </p>
+            <Link
+              to="/dev-spot"
+              className="w-[146px] h-[40px] bg-gradient-to-b from-[#505050] to-[#030404]  border-b-[1px] border-[#FFFFFFBD] flex  items-center justify-center rounded-[13px]">
+              View Devs
+            </Link>
+          </div>
+          <div className="flex sm:items-end justify-baseline sm:w-[35%] w-full sm:pr-[70px] pr-[0px] relative ">
+            <div className="absolute sm:top-1/5 top-[100px] sm:left-0 left-1/2 sm:-translate-x-0 -translate-x-1/2  sm:w-fit w-[90%]  h-fit  bg-black rounded-[34px] p-[27px]">
+              <img
+                src="/hey.png"
+                alt=""
+                className="w-[238px] h-[484px]  object-cover rounded-[20px] mx-auto"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Devs;
