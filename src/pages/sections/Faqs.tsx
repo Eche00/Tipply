72f9621141b0e9159e540c38b9720d2e521aa107
Eche00 <echeeze956@gmail.com>
2025-08-13 22:01:23 +0100

    Faqs page animation

--- src/pages/sections/Faqs.tsx
+++ src/pages/sections/Faqs.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Icons } from "../../lib/icons/Icons"
 import { Svgs } from "../../lib/svg/Svg"
+import { motion } from "framer-motion";
 
 function Faqs() {
   const[faq1,setFaq1] = useState(false)
@@ -27,11 +28,23 @@ function Faqs() {
 
 
        <section className="flex flex-col w-[90%] mx-auto py-[50px] z-30 relative ">
-         <h2 className="  text-[40px]  font-extrabold leading-[55px] capitalize bg-gradient-to-r from-[#008CFF] to-[#FFFFFF69] bg-clip-text text-transparent text-center">Frequently asked questions</h2>
-         <p className=" text-[#AFB3CA] text-[16px] font-[400] leading-[24px] text-center">Discover Devs, access Projects and tip with you fully in control.</p>
+         <motion.h2 className="  text-[40px]  font-extrabold leading-[55px] capitalize bg-gradient-to-r from-[#008CFF] to-[#FFFFFF69] bg-clip-text text-transparent text-center" initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>Frequently asked questions</motion.h2>
+         <motion.p className=" text-[#AFB3CA] text-[16px] font-[400] leading-[24px] text-center" initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>Discover Devs, access Projects and tip with you fully in control.</motion.p>
 
  <div className="flex xl:flex-row flex-col-reverse items-center">
-   <span className=" xl:rotate-[90deg] xl:inline-block hidden">{Svgs.infinity}</span>
+   <motion.span className=" xl:rotate-[90deg] xl:inline-block hidden" initial={{ scale: 0.8, opacity: 0 }}
+whileInView={{ scale: 1, opacity: 1 }}
+transition={{
+  type: "spring",   
+  bounce: 0.5,      
+  duration: 0.8,    
+  delay: 0.3        
+}}
+viewport={{ once: true }}>{Svgs.infinity}</motion.span>
          <section className="flex flex-col gap-[32px] w-full max-w-[90%] sm:max-w-[1200px] mx-auto py-[80px]">
           {/* faq1  */}
            <div className=" w-full bg-[#56565633] rounded-[24px] text-[#AFB3CA] sm:text-[24px] text-[16px] font-[400]   p-[20px] flex flex-col gap-[10px]">
