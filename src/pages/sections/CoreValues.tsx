8890c14b835c1814d7b082c30c01f1a3ecd95edb
Eche00 <echeeze956@gmail.com>
2025-08-13 21:50:08 +0100

    Adding animations to core value page

--- src/pages/sections/CoreValues.tsx
+++ src/pages/sections/CoreValues.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router"
 import { Icons } from "../../lib/icons/Icons"
+import { motion } from "framer-motion";
 
 function CoreValues() {
   return (
@@ -20,34 +21,46 @@ function CoreValues() {
 
      <section className="flex flex-col w-[90%] mx-auto py-[50px]">
       <div className="flex sm:flex-row flex-col sm:items-start items-center sm:justify-between justify-center">
-         <h2 className="  text-[40px]  font-[400] leading-[55px] capitalize bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF69] bg-clip-text text-transparent text-center">Core Values</h2>
+         <motion.h2 className="  text-[40px]  font-[400] leading-[55px] capitalize bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF69] bg-clip-text text-transparent text-center" initial={{ x: -50, opacity: 0 }}
+          whileInView={{ x: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>Core Values</motion.h2>
 
-        <p className=" text-[#AFB3CA] text-[16px] font-[400] leading-[24px] sm:text-start text-center max-w-[400px]">We believe in transparency, innovation, and community-driven growth. Our commitment is to empower developers and supporters alike with privacy, security, and seamless interactions across the Web3 ecosystem.</p>
+        <motion.p className=" text-[#AFB3CA] text-[16px] font-[400] leading-[24px] sm:text-start text-center max-w-[400px]" initial={{ scale: 0.9, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>We believe in transparency, innovation, and community-driven growth. Our commitment is to empower developers and supporters alike with privacy, security, and seamless interactions across the Web3 ecosystem.</motion.p>
       </div>
 
       <section className="flex  flex-wrap gap-[48px] items-stretch justify-center py-[48px] ">
         {/* mission  */}
-        <div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]">
+        <motion.div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]" initial={{ y: 40,scale:0.8, opacity: 0 }}
+          whileInView={{ y: 1,scale:1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>
           <span>{Icons.home}</span>
           <h3 className="text-[24px] text-[#FFFFFF] font-[400]">Mission</h3>
           <p className="max-w-[325px] text-[16px] text-[#AFB3CA] font-[400]">To connect tippers and developers worldwide by fostering early engagement and vibrant community growth. Tipply creates rewarding, seamless experiences that empower users to support and explore innovative Web3 projects while shaping the future of decentralized tipping.</p>
-        </div>
+        </motion.div>
         {/* vission  */}
-        <div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]">
+        <motion.div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]" initial={{ y: 40,scale:0.8, opacity: 0 }}
+          whileInView={{ y: 1,scale:1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>
           <span>{Icons.vision}</span>
           <h3 className="text-[24px] text-[#FFFFFF] font-[400]">Vision</h3>
           <p className="max-w-[325px] text-[16px] text-[#AFB3CA] font-[400]">To become the leading decentralized tipping platform that empowers a global community of developers and supporters, driving widespread adoption of Web3 technologies through trust, transparency, and effortless interaction.</p>
-        </div>
+        </motion.div>
         {/* support  */}
-        <div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]">
+        <motion.div className=" bg-[#141414] p-[24px] rounded-[24px] flex flex-col gap-[16px]" initial={{ y: 40,scale:0.8, opacity: 0 }}
+          whileInView={{ y: 1,scale:1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>
           <span>{Icons.people}</span>
           <h3 className="text-[24px] text-[#FFFFFF] font-[400]">Connect. Support. Grow.</h3>
           <p className="max-w-[325px] text-[16px] text-[#AFB3CA] font-[400]">At Tipply, we believe the power of Web3 comes from people coming together. Connect with talented developers, support innovative projects through seamless tipping, and grow a vibrant community that drives the future of decentralized collaboration. Together, we create more than just technology, we build lasting impact.</p>
-        </div>
+        </motion.div>
         
       </section>
        {/* Discover & Tip */}
-          <div className="flex sm:flex-row flex-col gap-[20px] bg-[#141414] rounded-[24px] sm:h-full sm:max-h-[377px] h-[752px] overflow-hidden ">
+          <motion.div className="flex sm:flex-row flex-col gap-[20px] bg-[#141414] rounded-[24px] sm:h-full sm:max-h-[377px] h-[752px] overflow-hidden " initial={{ scale: 0.8, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}  viewport={{once:true}}>
            <div className="flex flex-col gap-[16px] sm:py-[61px] py-[30px] sm:pl-[70px] sm:pr-0 pr-[30px] pl-[30px] sm:flex-1 w-full" >
              <p className="bg-[#FFFFFF0A] px-[10px] py-[8px] border-[1px] shadow-sm  border-[#FFFFFF14] text-[#FFFFFF] text-[12px] font-extrabold leading-[16px] flex items-center justify-center gap-[10px] rounded-full uppercase w-[117px]">DevSpot</p>
              <h3 className=" text-[#CAD1E9] text-[32px] font-[400]">Discover & Tip</h3>
@@ -60,7 +73,7 @@ function CoreValues() {
              </div>
 
            </div>
-          </div>
+          </motion.div>
     </section>
     </div>
   )
