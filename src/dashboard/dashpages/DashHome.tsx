73cc455104c7db70dbd5b1dfe7cd92073b69ad4d
Eche00 <echeeze956@gmail.com>
2025-08-16 22:14:52 +0100

    dashboard home section division

--- src/dashboard/dashpages/DashHome.tsx
+++ src/dashboard/dashpages/DashHome.tsx
@@ -1,7 +1,20 @@
 
 function DashHome() {
   return (
-    <div>DashHome</div>
+    <div className="bg-[#141718]  h-[98%] rounded-[16px] overflow-hidden flex flex-col gap-[20px] p-[20px]">
+      {/* first section  */}
+      <section className="flex sm:flex-row flex-col gap-[20px] flex-1  w-full">
+        <div className="flex flex-1 bg-[#034FE3] rounded-[16px]">22</div>
+        <div className="flex flex-1 bg-[#034FE3] rounded-[16px]">22</div>
+        <div className="flex flex-1 bg-[#034FE3] rounded-[16px]">22</div>
+      </section>
+      {/* second section  */}
+      <section className="flex sm:flex-row flex-col gap-[20px] flex-1  w-full">
+        <div className="flex flex-1 bg-[#034FE3] rounded-[16px]">22</div>
+        <div className="flex w-[32.5%] bg-[#034FE3] rounded-[16px]">22</div>
+        
+      </section>
+    </div>
   )
 }
 
