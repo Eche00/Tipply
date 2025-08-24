529b029121be046f4460e68743717d66395b10bd
Eche00 <echeeze956@gmail.com>
2025-08-24 17:44:01 +0100

    scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
-    console.log("Scrolled to top on:", pathname);
     window.scrollTo(0, 0);
   }, [pathname]);
   return null;
