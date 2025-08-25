315ae7aa0042e1541cf2b942d9816993a0827776
Eche00 <echeeze956@gmail.com>
2025-08-25 01:23:25 +0100

    Worked on dash conrtainer to add bottom vanbar, add drpdown and minor edits

--- src/dashboard/DashContainer.tsx
+++ src/dashboard/DashContainer.tsx
@@ -4,11 +4,15 @@ import Sidebar from "./Sidebar";
 import { Icons } from "../lib/icons/Icons";
 import { Close } from "@mui/icons-material";
 import { auth } from "../lib/firebase";
+import BottomNav from "./BottomNav";
+import GeneralNav from "../components/GeneralNav";
+import { headerLogics } from "../lib/logics/headerLogic";
 
 
 function DashContainer() {
   const [compress, setCompress] = useState(false);
-  const [hide, setHide] = useState(false);
+   const {dropDown,setDropDown} = headerLogics()
+ 
  const navigate = useNavigate()
 
   useEffect(()=>{
@@ -22,25 +26,36 @@ function DashContainer() {
   },[auth])
 
   return (
-    <div className="h-[100vh] relative sm:h-full bg-[#050520] w-full">
+    <div className="h-[100vh] relative sm:h-full bg-[#050520] w-full ">
       
-        <div className="w-[90%] mx-auto flex items-end justify-end py-[10px]">
-          <span onClick={()=>setHide(!hide)} className=" md:hidden flex z-50">{hide ? Icons.menu : <Close/>}</span>
+        <div className="flex items-end justify-end fixed top-0 left-0 w-full z-50 py-[25px] pr-5 bg-[#050520]/20 backdrop-blur-[24.7px] md:hidden">
+          <span onClick={()=>setDropDown(!dropDown)}  >{!dropDown ? Icons.menu : <Close/>}</span>
         </div>
       <section className=" w-full flex  relative ">
-       {!hide && 
-        <div className="flex fixed left-0 sm:left-[20px] top-[20px] bottom-[20px] z-50">
+      
+        <div className="md:flex hidden fixed left-0 sm:left-[20px] top-[20px] bottom-[20px] z-50">
           <Sidebar compress={compress} setCompress={setCompress} />
-        </div>}
+        </div>
+       
         <div
           className={
             compress
               ? "ml-0 md:ml-[80px] flex-1 sm:p-5 h-[100vh]  "
               : "ml-0 md:ml-[300px] flex-1 sm:p-5 h-[100vh]  "
           }>
+            <div className="sm:pb-0 py-[90px]">
           <Outlet />
+            </div>
+
+       {/* Bottom Navbar  */}
+          <section className="  fixed sm:hidden flex bottom-[20px] w-full">
+        <BottomNav />
+      </section>
         </div>
       </section>
+
+      {dropDown &&    
+       <GeneralNav/>}
     </div>
   );
 }
